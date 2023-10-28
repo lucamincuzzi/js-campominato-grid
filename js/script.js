6bac46ae4fee42b2deee9714b97823f06d5bff77
lucamincuzzi <luca.mincuzzi@outlook.it>
2023-10-28 17:20:15 +0200

    add: CSS & JS - difficulty settings implemented

--- js/script.js
+++ js/script.js
@@ -1,24 +1,55 @@
+// Codice di esecuzione
 const playButton = document.querySelector(".play");
 playButton.addEventListener("click", function() {
-    const grid = document.querySelector(".grid");
-    grid.classList.remove("hidden");
-    generateCells(100);
+  const difficultyLevel = document.querySelector(".challenge-selection").value;
+  console.log(difficultyLevel);
+  let number = 0;
+  // eseguo un controllo basato sulla difficoltà impostata nel select
+  // cambio di conseguenza il numero parametro della funzione generatrice delle celle a seconda dell'esito
+  if (difficultyLevel === "easy") {
+    number = 100;
+    generateCells(number);
+  } else if (difficultyLevel === "hard") {
+    number = 81;
+    generateCells(number);
+  } else {
+    number = 49;
+    generateCells(number);
+  }
+  // console log del numero risultante del controllo
+  console.log(number);
+  // catturo l'elemento HTML della griglia
+  const grid = document.querySelector(".grid");
+  // lo rendo visibile in pagina
+  grid.classList.remove("hidden");
+  // resetto il suo contenuto per evitare di accumulare griglie
+  grid.innerHTML = "";
+  // genero e inserisco le celle mediante la funzione
+  generateCells(number);
 })
 
 /******/
+// Funzioni
 /**
- * Dato un numero, la funzione genera elementi div con al loro interno numeri da 1 a x in successione, aggiunge loro un event listener e li inserisce nella griglia
+ * Dato un numero, la funzione genera tramite ciclo for elementi div con al loro interno numeri da 1 a x in successione, aggiunge loro un event listener e li inserisce nella griglia
  * @param {number}
  * @returns {none}
  */
 function generateCells(number) {
   for (let i = 1; i <= number; i++) {
+    // creo l'elemento HTML della cella
     cell = document.createElement("div");
-    cell.classList.add(`cell-${number}`);
+    cell.classList.add("cell");
+    // aggiungo la classe che determina la width delle celle
+    cell.classList.add(`nCell-${number}`);
+    // inserisco il numero in iterazione nella cella
     cell.innerHTML = i;
+    // aggiungo l'event listener per colorare la cella
     cell.addEventListener("click", whatNumber);
+    // aggiungo la cella creata nella griglia
     grid = document.querySelector(".grid");
     grid.append(cell);
+    // fine del ciclo
   }
 }
 
@@ -28,6 +59,8 @@ function generateCells(number) {
  * @returns {none}
  */
 function whatNumber() {
-    this.classList.add("aqua");
-    console.log(this.innerHTML);
+  // aggiungo la classe che colora la cella
+  this.classList.add("aqua");
+  // console log del numero contenuto nella cella
+  console.log(this.innerHTML);
 }
